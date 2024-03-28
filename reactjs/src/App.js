5bd4d36f4b61677aca03237c2c66887b884c13b2
EJGDLG <edwinjosegabriel@gmail.com>
2024-03-27 23:21:36 -0600

    log

---
+++ reactjs/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+
+<div class="container" id="container">
+        <div class="form-container sign-in">
+            <form>
+                <h1>Sign In</h1>
+                <span>or use your email password</span>
+                <input type="email" placeholder="Email"></input>
+                <input type="password" placeholder="Password"></input>
+                <a href="#">Forget Your Password?</a>
+                <button>Sign In</button>
+            </form>
+        </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
